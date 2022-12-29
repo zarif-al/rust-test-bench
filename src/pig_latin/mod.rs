80fbca89c9dcb83c531097f2af20a68265c53cc0
zarif-al <zarif_al96@outlook.com>
2022-12-29 02:50:31 +0000

    Create package to translate to pig latin

---
+++ src/pig_latin/mod.rs
@@ -0,0 +1,14 @@
+use std::fmt::format;
+
+pub fn get_pig_latin_translation(text: &String) -> String {
+    const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+    let char_vec: Vec<char> = text.chars().collect();
+    let starts_with_vowel = VOWELS.iter().any(|&c| c == char_vec[0]);
+    if starts_with_vowel {
+        let new_string = format!("{}-{}", &text, "hay");
+        return new_string;
+    } else {
+        let new_string = format!("{}-{}{}", &text[1..], char_vec[0], "ay");
+        return new_string;
+    }
+}

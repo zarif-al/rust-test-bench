b49b315a60a5a8a226b4dc75f0a70994255b564d
zarif-al <zarif_al96@outlook.com>
2022-12-30 16:51:31 +0000

    Comment unused code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-mod mean_and_median;
-mod pig_latin;
-use crate::mean_and_median::{ MeanAndMedian, get_mean_and_median };
-use crate::pig_latin::get_pig_latin_translation;
+/* mod mean_and_median;
+mod pig_latin; */
+mod employee_management;
+
+/* use crate::mean_and_median::{ MeanAndMedian, get_mean_and_median };
+use crate::pig_latin::get_pig_latin_translation; */
+use crate::employee_management::employee_management_system;
 
 fn main() {
-    let result: MeanAndMedian = get_mean_and_median([4, 1, 2, 3, 6, 8, 7]);
+    /*  let result: MeanAndMedian = get_mean_and_median([4, 1, 2, 3, 6, 8, 7]);
     println!("");
     println!("Mean, Median and Mode");
-    println!("Mean: {}, Median: {}", result.mean, result.median);
+    println!("Mean: {}, Median: {}", result.mean, result.median); */
 
-    let text_one = String::from("Apple");
+    /*  let text_one = String::from("Apple");
     let text_two: String = String::from("first");
     println!("");
     println!("Pig Latin Translation");
     println!("{:?}", get_pig_latin_translation(&text_one));
     println!("{:?}", get_pig_latin_translation(&text_two));
 
-    println!("");
+    println!(""); */
     println!("Employee Management System");
+    employee_management_system();
 }

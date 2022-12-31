9d9a98f04729586ab708842b6dc281fbe9f0697d
zarif-al <zarif_al96@outlook.com>
2022-12-31 12:34:27 +0000

    Use take_numerical_input function

--- src/employee_management/view_employees/mod.rs
+++ src/employee_management/view_employees/mod.rs
@@ -1,40 +1,20 @@
 use std::collections::HashMap;
 use std::io;
-
+use crate::numerical_input::take_numberical_input;
 // TODO: Create two specific functions.
 // view_all_employees -> View all employees by departments sorted.
 // view_employees_by_department -> View employees of a specified department.
 
 pub fn view_employees(departments: &Vec<String>, employees: &HashMap<String, String>) {
-    let mut input = String::new();
-    let options_input: i32;
+    let user_input: u32;
 
     println!("\nHow would like to view the employee list?\n");
     println!("0. All employees in a specific department.");
     println!("1. All employees by department.");
 
-    loop {
-        input.clear();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
+    user_input = take_numberical_input(1);
 
-        let user_input: i32 = match input.trim().parse() {
-            Err(_) => {
-                println!("\nPlease input a valid number\n");
-                continue;
-            }
-            Ok(num) => { num }
-        };
-
-        if user_input > 1 {
-            println!("\nPlease input a valid option\n");
-            continue;
-        }
-
-        options_input = user_input;
-        break;
-    }
-
-    if options_input == 0 {
+    if user_input == 0 {
         view_employees_by_department(departments, employees);
     }
 }

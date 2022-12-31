536ce5f9ecf4670cf75f620f4ac5da80da920ff8
zarif-al <zarif_al96@outlook.com>
2022-12-31 12:45:15 +0000

    Use take_numberical_input method

--- src/employee_management/add_employee/mod.rs
+++ src/employee_management/add_employee/mod.rs
@@ -1,13 +1,13 @@
 use std::convert::TryFrom;
 use std::collections::HashMap;
 use std::io;
+use crate::numerical_input::take_numberical_input;
 
-pub fn add_employee(employees: &mut HashMap<String, String>, departments: &Vec<String>) -> u32 {
+pub fn add_employee(employees: &mut HashMap<String, String>, departments: &Vec<String>) {
     // Declare variables
-    let department_length = i32::try_from(departments.len()).unwrap_or(0);
+    let department_length = u32::try_from(departments.len()).unwrap_or(0);
     let mut employee_name = String::new();
-    let mut department_input = String::new();
-    let department_index: i32;
+    let user_input: u32;
 
     // Ask for user input
     println!("\nPlease enter the employee name or enter 0 to exit.\n");
@@ -23,7 +23,7 @@ pub fn add_employee(employees: &mut HashMap<String, String>, departments: &Vec<S
 
     // If the number is 0 then exit with 0
     if exit_command == 0 {
-        return 0;
+        return;
     }
 
     // Check if employee exists
@@ -35,79 +35,35 @@ pub fn add_employee(employees: &mut HashMap<String, String>, departments: &Vec<S
         println!("\nThis employee already exists! Would you like to modify their department?\n");
         println!("0. Yes");
         println!("1. No");
-        let selected_user_option: u32;
+        let user_input: u32;
 
-        // Loop until a valid user response is received.
-        loop {
-            let mut user_input = String::new();
-            io::stdin().read_line(&mut user_input).expect("Failed to read line");
-            user_input = user_input.trim().to_string();
-
-            let parsed_user_input: u32 = match user_input.parse() {
-                Err(_) => {
-                    println!("\nPlease input a valid number\n");
-                    continue;
-                }
-                Ok(num) => { num }
-            };
-
-            if parsed_user_input > 1 {
-                println!("\nPlease input a valid option\n");
-                continue;
-            }
-
-            selected_user_option = parsed_user_input;
-            break;
-        }
+        user_input = take_numberical_input(1);
 
         // If user selects "No" then exit.
-        if selected_user_option == 1 {
-            return 0;
+        if user_input == 1 {
+            return;
         }
     }
 
-    // Start a loop to take deparment index input
-    loop {
-        // Ask the user to select a department.
-        println!("\nPlese select a department.");
+    // List available departments
+    println!("\nPlese select a department.");
+    for (i, el) in departments.iter().enumerate() {
+        println!("{}: {}", i, el);
+    }
 
-        // List available departments
-        for (i, el) in departments.iter().enumerate() {
-            println!("{}: {}", i, el);
-        }
+    user_input = take_numberical_input(department_length);
 
-        department_input.clear();
-        io::stdin().read_line(&mut department_input).expect("Failed to read line");
+    let binding = String::from("Null");
+    let selected_department = departments
+        .get(user_input as usize)
+        .unwrap_or(&binding)
+        .to_string();
 
-        // Reject invalid number input and restart current loop
-        let user_input: i32 = match department_input.trim().parse() {
-            Err(_) => {
-                println!("\nPlease input a valid number\n");
-                continue;
-            }
-            Ok(num) => { num }
-        };
-
-        // Reject invalid range input and restart the loop
-        if user_input >= department_length {
-            println!("\nPlease input a valid number\n");
-            continue;
-        }
-
-        // Assign a valid department index and break loop.
-        department_index = user_input;
-        break;
+    if selected_department == binding {
+        println!("\nThere was an error processing your request.\n");
+        return;
     }
 
     // Insert employee into employees Hash Map
-    employees.insert(
-        employee_name.trim().to_string(),
-        departments
-            .get(department_index as usize)
-            .unwrap_or(&String::from("None"))
-            .to_string()
-    );
-
-    // Exit with 1
-    1
+    employees.insert(employee_name.trim().to_string(), selected_department);
 }

1f749fe521cc01194526c9a982df9ed242143c29
zarif-al <zarif_al96@outlook.com>
2023-01-01 02:49:22 +0000

    Create a function to sort all employees by department and print

--- src/employee_management/view_employees/mod.rs
+++ src/employee_management/view_employees/mod.rs
@@ -1,8 +1,5 @@
 use std::collections::HashMap;
 use crate::custom_input::take_numberical_input;
-// TODO: Create two specific functions.
-// view_all_employees -> View all employees by departments sorted.
-// view_employees_by_department -> View employees of a specified department.
 
 pub fn view_employees(departments: &Vec<String>, employees: &HashMap<String, String>) {
     let user_input: u32;
@@ -15,6 +12,10 @@ pub fn view_employees(departments: &Vec<String>, employees: &HashMap<String, Str
 
     if user_input == 0 {
         view_employees_by_department(departments, employees);
+    }
+
+    if user_input == 1 {
+        view_all_employees(employees);
     }
 }
 
@@ -44,5 +45,15 @@ fn view_employees_by_department(departments: &Vec<String>, employees: &HashMap<S
         if v == &selected_department {
             println!("{}", k);
         }
+    }
+}
+
+fn view_all_employees(employees: &HashMap<String, String>) {
+    let mut employee_vector: Vec<(&String, &String)> = employees.into_iter().collect();
+
+    employee_vector.sort_by_key(|k| k.1);
+
+    for item in employee_vector {
+        println!("{}, {}", item.0, item.1);
     }
 }
